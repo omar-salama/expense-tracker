f61cc6a15cb471bf160fbc814d8354f1853ba227
Omar Salama <omar.salama@exas2030.com>
2025-06-30 14:14:39 +0300

    fix: update Swagger documentation path to 'docs' with global prefix

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
