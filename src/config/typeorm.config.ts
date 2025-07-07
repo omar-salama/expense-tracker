be07ad0aac37d826a6a77860950b63317386dfae
Omar Salama <omar.salama@exas2030.com>
2025-07-07 16:14:34 +0300

    feat(app): dockerized the entire app and make use of pgAdmin

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,11 +3,11 @@ import { ConfigService } from '@nestjs/config';
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
+  host: configService.get<string>('POSTGRES_HOST'),
+  port: configService.get<number>('POSTGRES_PORT'),
+  username: configService.get<string>('POSTGRES_USERNAME'),
+  password: configService.get<string>('POSTGRES_PASSWORD'),
+  database: configService.get<string>('POSTGRES_DB'),
   autoLoadEntities: true,
   synchronize: true, // Not recommended for production
 }); 
